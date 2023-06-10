5ff35c32ca985d590d38a34cfa9be4649c642764
Neelam Dubey <ashidubey8208@gmail.com>
2023-06-10 16:37:00 +0530

    Second try

---
+++ src/main/java/com/driver/model/Admin.java
@@ -0,0 +1,40 @@
+package com.driver.model;
+
+import javax.persistence.*;
+
+
+//@Table
+@Entity
+public  class Admin {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer adminId;
+
+    private String username;
+    private String password;
+
+    public Integer getAdminId() {
+        return adminId;
+    }
+
+    public void setAdminId(Integer adminId) {
+        this.adminId = adminId;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
